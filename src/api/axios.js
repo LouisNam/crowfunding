bac7ae80a1eaca835500e9fbd34d0147bbb26173
Nam Do <dotranthainam@gmail.com>
2023-04-09 06:27:57 +0700

    403

--- src/api/axios.js
+++ src/api/axios.js
@@ -7,10 +7,10 @@ export default axios.create({
     "Cache-Control": "no-cache",
   },
 });
-// export const axiosPrivate = axios.create({
-//   baseURL: "http://localhost:3009",
-//   headers: {
-//     "Content-Type": "application/json",
-//     "Cache-Control": "no-cache",
-//   },
-// });
+export const axiosPrivate = axios.create({
+  baseURL: "http://localhost:4001",
+  headers: {
+    "Content-Type": "application/json",
+    "Cache-Control": "no-cache",
+  },
+});
